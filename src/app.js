cb50fd206d58ae7b1f9530b94a546473ccc6e7d0
Winniee356 <92734149+Winniee356@users.noreply.github.com>
2021-12-06 16:21:26 +0100

    Added Date, Time and Months

--- src/app.js
+++ src/app.js
@@ -1,3 +1,49 @@
+let currentTime = new Date();
+
+let fullDate = document.querySelector("#current-date");
+fullDate.innerHTML = formatDate(currentTime);
+function formatDate(date) {
+  let days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let day = date.getDay();
+  let currentDay = days[day - 1];
+  let currentHours = date.getHours();
+  if (currentHours < 10) {
+    currentHours = `0${currentHours}`;
+  }
+  let currentMinutes = date.getMinutes();
+  if (currentMinutes < 10) {
+    currentMinutes = `0${currentMinutes}`;
+  }
+  let currentMonth = months[date.getMonth()];
+  let currentDate = date.getDate();
+
+  let formattedDate = `${currentDay} ${currentHours}:${currentMinutes} </br> ${currentDate} ${currentMonth}`;
+  return formattedDate;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
