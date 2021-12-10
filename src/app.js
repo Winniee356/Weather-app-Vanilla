7b93a1293defe6eba50c1faae175b466a95f4a33
Winniee356 <92734149+Winniee356@users.noreply.github.com>
2021-12-10 17:13:04 +0100

    start API integration of forecast

--- src/app.js
+++ src/app.js
@@ -44,7 +44,8 @@ function formatDate(date) {
   return formattedDate;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   forecastHTML = `<div class="row">`;
@@ -74,6 +75,14 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "439cdfb8be6db01f862da6d16b1a4363";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
@@ -91,9 +100,9 @@ function displayTemperature(response) {
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
-
-  displayForecast();
   celciusTemperature = response.data.main.temp;
+
+  getForecast(response.data.coord);
 }
 
 function searchCity(city) {
